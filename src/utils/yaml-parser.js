607e8f0a001f8a4e74694b9d0a46638b3d1013a4
Yura Myhaylyak <ghost86421@gmail.com>
2019-10-15 19:39:47 +0300

    Deleted `d3-selection` package; Added yaml parser

--- src/utils/yaml-parser.js
+++ src/utils/yaml-parser.js
@@ -1,18 +1,41 @@
-export const parse = (yamlString = '') => {
-  const entries = yamlString.split(/\n/).map(p => p.split(/:\s?/));
+const EMPTY_STRING = '';
 
-  return Object.fromEntries(entries);
+/**
+ * Parse YAML into JSON
+ *
+ * @param {string} [input=EMPTY_STRING]
+ * @returns {object}
+ */
+export const parse = (input = EMPTY_STRING) => {
+  const accumulator = {};
+  const parts = input
+    .replace(/^\s/, EMPTY_STRING)
+    .replace(/\s$/, EMPTY_STRING)
+    .split(/\n/)
+
+  for (const [index, row] of parts.entries()) {
+    const [key, value] = row.split(':');
+    const nestedItem = key.startsWith(' ');
+
+    if (nestedItem) {
+      continue;
+    }
+
+    if (!value) {
+      const childElements = parts.slice(index + 1);
+      const initialChildsBarrier = childElements.findIndex(e => /^\S/.test(e));
+      const computedChildElementsBarrier = initialChildsBarrier === -1 ? childElements.length : initialChildsBarrier;
+
+      accumulator[key] = parse(
+        childElements
+          .slice(0, computedChildElementsBarrier)
+          .join('\n')
+          .replace(/^\s{2}/gm, EMPTY_STRING),
+      )
+    } else {
+      accumulator[key] = value.slice(1);
+    }
+  }
+
+  return accumulator;
 };
-
-
-```
-name: Example
-description: Displays a Title, Image, and Text on the front end.
-type: particle
-
-title:
-type: input.checkbox
-label: Your label.
-  description: Your description.
-    default: true
-```
