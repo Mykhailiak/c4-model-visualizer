0bf7513db7a16103b5d44b93ad7abac574b47309
Yura Myhaylyak <ghost86421@gmail.com>
2020-01-16 17:00:40 +0200

    Made TextContentEditor controlled component

--- src/components/TextContentEditor.js
+++ src/components/TextContentEditor.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ContentStatus from './ContentStatus';
 
 const DELAY = 3000;
@@ -9,19 +9,48 @@ export default ({
   setUpdatingState,
   status,
 }) => {
+  const [text, setText] = useState();
   const onChangeHandler = (e) => {
     const { value } = e.target;
 
+    setText(value);
     clearTimeout(timerId);
     setUpdatingState(true);
     timerId = setTimeout(() => updateState(value), DELAY);
   };
 
+  const onKeyUp = (e) => {
+    e.persist();
+
+    if (e.keyCode === 13) {
+      setText((value) => {
+        const { selectionStart } = e.target;
+        const end = selectionStart - 2;
+        const headSpaces = value
+          .slice(value.lastIndexOf('\n', end), end)
+          .replace(/\n/g, '')
+          .match(/^\s+/);
+        const parentOffset = (headSpaces || [''])[0].length;
+        // const newSelectionStartPosition = selectionStart + parentOffset;
+        // e.target.setSelectionRange(newSelectionStartPosition, newSelectionStartPosition + 1);
+
+        const result = value
+          .slice(0, selectionStart)
+          .concat(' '.repeat(parentOffset + 2))
+          .concat(value.slice(selectionStart));
+
+        return result;
+      });
+    }
+  };
+
   return (
     <>
       <textarea
+        value={text}
         className="text-content-editor"
         onChange={onChangeHandler}
+        onKeyUp={onKeyUp}
       />
       <ContentStatus type={status} />
     </>
