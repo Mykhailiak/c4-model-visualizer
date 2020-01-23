e42f668616bc1b94af95f197bd24e357ceb1b7db
Yura Myhaylyak <ghost86421@gmail.com>
2020-01-23 14:15:57 +0200

    Added tests for `ContentStatus` component

---
+++ src/__tests__/ContentStatus.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Alert } from 'antd';
+import ContentStatus from '../components/ContentStatus';
+
+it('displays success message if provided `type` is `success`', () => {
+  const contentStatus = shallow(<ContentStatus type="success" />);
+  const alert = (
+    <Alert
+      className="content-status-alert"
+      type="success"
+      message="Valid input"
+      showIcon
+    />
+  );
+
+  expect(contentStatus.contains(alert)).toEqual(true);
+});
+
+it('displays error message if provided `type` is `error`', () => {
+  const contentStatus = shallow(<ContentStatus type="error" />);
+  const alert = (
+    <Alert
+      className="content-status-alert"
+      type="error"
+      message="Syntax error"
+      showIcon
+    />
+  );
+
+  expect(contentStatus.contains(alert)).toEqual(true);
+});
