2255db4a8ca3db092f1f3983140a0899aa0e485d
Yura Myhaylyak <ghost86421@gmail.com>
2020-01-23 12:51:35 +0200

    Added jsdoc for c4-input-validator

--- src/utils/c4-input-validator.js
+++ src/utils/c4-input-validator.js
@@ -1,3 +1,9 @@
+/**
+ * Validation parsed JSON according to C4 rules
+ *
+ * @param {object} [input={}]
+ * @returns {object}
+ */
 export default (input = {}) => {
   if (!input.context) {
     throw new SyntaxError('Context is mandatory!');
