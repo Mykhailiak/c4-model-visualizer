53a05b54888512149286e0390779b7603ce708a9
Yura Myhaylyak <ghost86421@gmail.com>
2020-02-01 15:10:30 +0200

    Added tests for `TextContentEditor` component

--- src/__tests__/TextContentEditor.test.js
+++ src/__tests__/TextContentEditor.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
 import TextContentEditor from '../components/TextContentEditor';
 
 it('calls `setUpdatingState` method during text updating', () => {
@@ -11,7 +12,75 @@ it('calls `setUpdatingState` method during text updating', () => {
     />,
   );
 
-  component.find('textarea').simulate('change', { target: { value } });
-
+  act(() => {
+    component.find('textarea').simulate('change', { target: { value } });
+  });
   expect(setUpdatingState).toHaveBeenCalledWith(true);
+});
+
+it('uses delay during uninterapted editing', () => {
+  jest.useFakeTimers();
+  const component = mount(
+    <TextContentEditor
+      status="success"
+      setUpdatingState={() => 1}
+      updateState={() => 1}
+    />,
+  );
+  const textarea = component.find('textarea');
+  expect(setTimeout).toHaveBeenCalledTimes(2);
+
+  act(() => {
+    textarea.simulate('change', { target: { value: 'text' } });
+  });
+  expect(setTimeout).toHaveBeenCalledTimes(3);
+
+  act(() => {
+    textarea.simulate('change', { target: { value: 'text-text' } });
+  });
+  expect(setTimeout).toHaveBeenCalledTimes(4);
+});
+
+it('calls `updateState` method once updating timeout is fired', () => {
+  jest.useFakeTimers();
+  const updateState = jest.fn();
+  const value = 'text';
+  const component = mount(
+    <TextContentEditor
+      status="success"
+      setUpdatingState={() => 1}
+      updateState={updateState}
+    />,
+  );
+
+  act(() => {
+    component.find('textarea').simulate('change', { target: { value } });
+  });
+  jest.runAllTimers();
+  expect(updateState).toHaveBeenCalledWith(value);
+});
+
+it('sets cursor position according to nesting', () => {
+  jest.useFakeTimers();
+  const component = mount(
+    <TextContentEditor
+      status="success"
+      setUpdatingState={() => 1}
+      updateState={() => 1}
+    />,
+  );
+  const textarea = component.find('textarea');
+
+  act(() => {
+    textarea.simulate('change', { target: { value: 'context:' } });
+    textarea.simulate('keyUp', {
+      target: {
+        value: 'context:',
+        selectionStart: 8,
+      },
+      keyCode: 13,
+    });
+  });
+
+  expect(textarea.text().length).toEqual(10);
 });
