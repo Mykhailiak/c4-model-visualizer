50d06219032609c2f4d1e74cc39aa1b287bb6376
Yura Myhaylyak <ghost86421@gmail.com>
2020-02-16 10:55:26 +0200

    Use core packae instead of own implementation; Updated test of `Diagram` component according to previous message

--- src/components/Diagram.js
+++ src/components/Diagram.js
@@ -1,142 +1,44 @@
 import React, { Component } from 'react';
-import cytoscape from 'cytoscape';
-import dagre from 'cytoscape-dagre';
+import DiagramVisualizer from 'c4-model-visualizer-core/diagram';
 import { getSuitableLevelKey, levels } from './LevelSelector';
-
-const style = [
-  {
-    selector: 'node',
-    style: {
-      width: 70,
-      label: 'data(name)',
-      shape: 'round-rectangle',
-      'font-size': 5,
-      'text-max-width': 70,
-      'text-valign': 'center',
-      'background-color': '#b3c2d8',
-      'text-wrap': 'ellipsis',
-    },
-  },
-  {
-    selector: ':parent',
-    style: {
-      'background-color': '#fff',
-      'background-opacity': 0.3,
-      'text-valign': 'top',
-      'text-halign': 'center',
-      'border-style': 'dashed',
-      'text-margin-y': -3,
-    },
-  },
-  {
-    selector: 'edge',
-    style: {
-      width: 4,
-      label: 'data(name)',
-      'font-size': 4,
-      'text-background-color': '#fff',
-      'text-background-padding': 3,
-      'text-background-opacity': 0.8,
-      'text-background-shape': 'round-rectangle',
-
-      'target-arrow-shape': 'triangle',
-      'line-color': '#cdd6e4',
-      'target-arrow-color': '#cdd6e4',
-      'curve-style': 'straight',
-      'line-cap': 'square',
-    },
-  },
-];
-
-cytoscape.use(dagre);
 
 export default class Diagram extends Component {
   componentDidMount() {
     const { props } = this;
+    this.diagram = new DiagramVisualizer(
+      levels,
+      getSuitableLevelKey,
+      {
+        containerId: 'cy',
+        onClick: (e) => {
+          const { selectionId, hasChild } = e.target.data();
 
-    this.layout = { name: 'dagre' };
-    this.cy = cytoscape({
-      style,
-      maxZoom: 6,
-      minZoom: 1,
-      userZoomingEnabled: true,
-      userPanningEnabled: true,
-      boxSelectionEnabled: false,
-      autounselectify: true,
-      container: document.getElementById('cy'),
-      elements: [],
-      layout: this.layout,
-    });
-
-    this.cy.on('click', 'node', (e) => {
-      const { selectionId, hasChild } = e.target.data();
-
-      if (hasChild) {
-        props.selectLevel(selectionId);
-      }
-    });
+          if (hasChild) {
+            props.selectLevel(selectionId);
+          }
+        },
+      },
+    );
   }
 
   componentDidUpdate() {
-    const { props, cy, layout } = this;
+    const { props } = this;
     const { context } = props.data;
     this.selectedPath = (props.selectedLevel || '').split(':');
     const elements = this.computeElements(context);
 
-    cy.json({ elements });
-    cy.ready(() => cy.layout(layout).run());
+    this.diagram.update(elements);
     this.fitViewport();
   }
 
   fitViewport() {
     const destination = this.selectedPath[this.selectedPath.length - 1];
 
-    if (destination) {
-      this.cy.fit(`#${destination}`);
-    }
+    this.diagram.fitViewport(destination);
   }
 
   computeElements(context = {}, parent, level = 0, selectionPath = levels[0]) {
-    const keys = Object.keys(context);
-
-    return keys
-      .reduce((acc, key) => {
-        let groups = [];
-        const { relations: { to: targetsSource } = {} } = context[key];
-        const validEdge = targetsSource && Object.keys(targetsSource).some((t) => keys.includes(t));
-        const node = context[key];
-        const name = node.name || key;
-        const selectionId = `${selectionPath}:${key}`;
-        const nodeContextKey = getSuitableLevelKey(node, level + 1);
-        const visibleNode = this.selectedPath.includes(key);
-        const hasChild = Boolean(nodeContextKey);
-
-        if (hasChild && visibleNode) {
-          groups = this.computeElements(node[nodeContextKey], key, level + 1, selectionId);
-        }
-
-        return acc
-          .concat({
-            data: {
-              name,
-              parent,
-              hasChild,
-              selectionId,
-              id: key,
-            },
-          })
-          .concat(
-            validEdge ? Object.keys(targetsSource).map((target) => ({
-              data: {
-                target,
-                id: `${key}_${target}`,
-                source: key,
-                name: targetsSource[target],
-              },
-            })) : [],
-          )
-          .concat(groups);
-      }, []);
+    return this.diagram.computeElements(context, parent, level, this.selectedPath, selectionPath);
   }
 
   render() {
