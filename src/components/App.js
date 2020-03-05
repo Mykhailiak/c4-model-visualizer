58b79b58d2026d06f8fce4ff29ce0cd9fe09fa28
Yurii Mykhailiak <ghost86421@gmail.com>
2020-03-05 21:52:28 +0200

    Reverted App component to be class-based

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
-import React, { useState } from 'react';
-import { Layout, Row, Col } from 'antd';
+import React, { Component } from 'react';
+import {
+  Layout,
+  Row,
+  Col,
+} from 'antd';
 import c4InputValidator from 'c4-model-visualizer-core/utils/c4-input-validator';
 import { parseAsync as parseYaml } from 'c4-model-visualizer-core/utils/yaml-parser';
 import TextContentEditor from './TextContentEditor';
@@ -7,56 +11,84 @@ import Diagram from './Diagram';
 import LevelSelector, { rootLevel } from './LevelSelector';
 import Sidebar from './Sidebar';
 
-const App = () => {
-  const [data, setData] = useState({});
-  const [parsingStatus, setParsingStatus] = useState(null);
-  const [selectedLevel, setSelectedLevel] = useState(null);
-  const [updatingContent, setUpdatingContent] = useState(false);
+class App extends Component {
+  constructor() {
+    super();
 
-  const updateData = (input) =>
-    parseYaml(input)
+    this.state = {
+      data: {},
+      parsingStatus: null,
+      selectedLevel: null,
+      updatingContent: false,
+    };
+
+    this.selectLevel = this.selectLevel.bind(this);
+    this.setUpdatingState = this.setUpdatingState.bind(this);
+    this.updateData = this.updateData.bind(this);
+  }
+
+  setUpdatingState(value = false) {
+    this.setState({ updatingContent: value });
+  }
+
+  updateData(input) {
+    return parseYaml(input)
       .then(c4InputValidator)
-      .then((processedData) => {
-        setData(processedData);
-        setParsingStatus('success');
-        setSelectedLevel(selectedLevel || rootLevel);
-        setUpdatingContent(false);
-      })
-      .catch(() => {
-        setData({});
-        setParsingStatus('error');
-        setSelectedLevel(null);
-        setUpdatingContent(false);
-      });
+      .then((data) => this.setState((state) => ({
+        data,
+        parsingStatus: 'success',
+        selectedLevel: state.selectedLevel || rootLevel,
+        updatingContent: false,
+      })))
+      .catch(() => this.setState({
+        parsingStatus: 'error',
+        data: {},
+        selectedLevel: null,
+        updatingContent: false,
+      }));
+  }
 
-  return (
-    <Layout className="app-layout">
-      <Sidebar updatingContent={updatingContent} />
-      <Layout.Content>
-        <Row type="flex">
-          <Col span={8} className="sidebar">
-            <TextContentEditor
-              updateState={updateData}
-              status={parsingStatus}
-              setUpdatingState={setUpdatingContent}
-            />
-          </Col>
-          <Col span={16}>
-            <LevelSelector
-              parsedYaml={data}
-              selectLevel={setSelectedLevel}
-              value={selectedLevel}
-            />
-            <Diagram
-              data={data}
-              selectLevel={setSelectedLevel}
-              selectedLevel={selectedLevel}
-            />
-          </Col>
-        </Row>
-      </Layout.Content>
-    </Layout>
-  );
-};
+  selectLevel(value) {
+    this.setState({ selectedLevel: value });
+  }
+
+  render() {
+    const {
+      parsingStatus,
+      data,
+      selectedLevel,
+      updatingContent,
+    } = this.state;
+
+    return (
+      <Layout className="app-layout">
+        <Sidebar updatingContent={updatingContent} />
+        <Layout.Content>
+          <Row type="flex">
+            <Col span={8} className="sidebar">
+              <TextContentEditor
+                updateState={this.updateData}
+                status={parsingStatus}
+                setUpdatingState={this.setUpdatingState}
+              />
+            </Col>
+            <Col span={16}>
+              <LevelSelector
+                parsedYaml={data}
+                selectLevel={this.selectLevel}
+                value={selectedLevel}
+              />
+              <Diagram
+                data={data}
+                selectLevel={this.selectLevel}
+                selectedLevel={selectedLevel}
+              />
+            </Col>
+          </Row>
+        </Layout.Content>
+      </Layout>
+    );
+  }
+}
 
 export default App;
